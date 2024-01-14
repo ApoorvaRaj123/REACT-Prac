79d2a77571a6fd767ad341eb6a548153cfdd50d8
ApoorvaRaj123 <99551949+ApoorvaRaj123@users.noreply.github.com>
2024-01-14 16:55:47 +0530

    Give props data to Navbar.jsx

--- react 5 - Props Exercise/src/components/Navbar.jsx
+++ react 5 - Props Exercise/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
-function Navbar({favouriteCount, count}) {
+function Navbar({data}) {
+
 
 
     return (
@@ -8,7 +9,7 @@ function Navbar({favouriteCount, count}) {
             <h3>Orange</h3>
             <div className="flex p-2 px-4 bg-orange-600 text-white rounded-md text-sm gap-3">
                 <h3>Favourites</h3>
-                <h4>{favouriteCount(count)}</h4>
+                <h4>{data.filter(item => item.added).length}</h4>
             </div>
         </div>
     )
