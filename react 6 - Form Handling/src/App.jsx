027bd79a1bb73b62e46910f02de647f830f5bfec
ApoorvaRaj123 <99551949+ApoorvaRaj123@users.noreply.github.com>
2024-01-20 16:51:34 +0530

    Add second way of form handling in App.jsx

--- react 6 - Form Handling/src/App.jsx
+++ react 6 - Form Handling/src/App.jsx
@@ -1,4 +1,4 @@
-import React, {useRef} from "react";
+import React, {useRef, useState} from "react";
 
 
 // ----------------------------------------------------------------FORM HANDLING ----------------------------------------------------------------
@@ -6,28 +6,58 @@ import React, {useRef} from "react";
 // ---------------------------------    WAY 1 - Using "useRef" ----------------------------------------------------------------
 
 
-function App(){
+// function App(){
 
-  const name = useRef(null);
-  const age = useRef(null);
+//   const name = useRef(null);
+//   const age = useRef(null);
 
 
-  const handleSubmit = (event)=>{
-    event.preventDefault();
-    console.log(name.current.value, age.current.value);
-  }
+//   const handleSubmit = (event)=>{
+//     event.preventDefault();
+//     console.log(name.current.value, age.current.value);
+//   }
 
-  return (
-  <>
-    <div className = "p-4">
-      <form action="" onSubmit={handleSubmit}>
-        <input ref={name} className="border-2 m-2" type="text" placeholder="name"/>
-        <input ref={age} className="border-2 m-2" placeholder="age" type="text" />
-        <input type="submit" />
-      </form>
-    </div>
-  </>
-  )
-}
+//   return (
+//   <>
+//     <div className = "p-4">
+//       <form action="" onSubmit={handleSubmit}>
+//         <input ref={name} className="border-2 m-2" type="text" placeholder="name"/>
+//         <input ref={age} className="border-2 m-2" placeholder="age" type="text" />
+//         <input type="submit" />
+//       </form>
+//     </div>
+//   </>
+//   )
+// }
+
+
+
+
+
+
+// ---------------------------------    WAY 2 - Using controlled components ----------------------------------------------------------------
+
+
+// function App(){
+
+
+//   const [val,setVal] = useState({name:"", email:""})
+
+//   const handleClick = (event) => {
+//     event.preventDefault();
+//   }
+
+//   return (
+//   <>
+//     <div className = "p-4">
+//       <form action="" onSubmit={handleClick}>
+//         <input onChange= {(event) => setVal({...val, name: event.target.value})} className="border-2 m-2" type="text" placeholder="name"/>
+//         <input onChange= {(event) => setVal({...val, email: event.target.value})} className="border-2 m-2" type="text" placeholder="name"/>
+//       </form>
+//     </div>
+//   </>
+//   )
+// }
+
 
 export default App;
