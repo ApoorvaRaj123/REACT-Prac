581985747d38df923dc3eb120576aee572a287e6
ApoorvaRaj123 <99551949+ApoorvaRaj123@users.noreply.github.com>
2024-02-18 15:53:27 +0530

    Add axios method in Services.jsx

--- react 9 - API integration/src/components/Services.jsx
+++ react 9 - API integration/src/components/Services.jsx
@@ -1,10 +1,52 @@
-import { React } from "react";
+import axios from "../utils/Axios.jsx";
+import { React, useEffect, useState } from "react";
 
 function Services() {
-    return (
-        <h1>Services</h1>
-    )
-}
+  const [first, setFirst] = useState("normal data");
+  const [second, setSecond] = useState("large data");
 
+
+  const getUsers = () => {
+    
+    axios
+      .get("/users")
+      .then((products) => {
+        console.log(products);
+        setProducts(products.data);
+      })
+      .catch((err) => console.log(err));
+  };
+
+
+  useEffect(() => {
+    console.log("Service component is created");
+
+    return () => {
+      console.log("service component is deleted");
+    };
+  });
+
+  return (
+    <>
+      <div>
+        <h1>{first}</h1>
+        <button
+          onClick={() => setFirst("Changed normal data")}
+          className="mb-10 bg-red-100 rounded p-2"
+        >
+          Change normal Data
+        </button>
+
+        <h1>{second}</h1>
+        <button
+          onClick={() => setFirst("Changed large data")}
+          className="mb-2 bg-red-100 rounded p-2"
+        >
+          Change Large Data
+        </button>
+      </div>
+    </>
+  );
+}
 
 export default Services;
