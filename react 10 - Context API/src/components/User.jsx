f21e61aa4e4f57d7d74c89646c64efebedd3692f
ApoorvaRaj123 <99551949+ApoorvaRaj123@users.noreply.github.com>
2024-02-21 17:06:59 +0530

    use useContext in User.jsx

--- react 10 - Context API/src/components/User.jsx
+++ react 10 - Context API/src/components/User.jsx
@@ -1,9 +1,20 @@
-import { React } from "react";
+import { React, useContext } from "react";
+import { Link } from "react-router-dom";
+import {UserContext} from "./Context.jsx"
 
 const User = () => {
+
+  const x = useContext(UserContext);
+  console.log(x);
   return (
+
     <>
-      <h1>User</h1>
+      <div>
+        <h1 className="text-3xl">User List</h1>
+        <div className="mt-10 bg-blue-300 w-1/2">
+          <Link className ="p-3">User name</Link>
+        </div>
+      </div>
     </>
   );
 };
