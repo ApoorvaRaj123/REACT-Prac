e4acd2d4402a4aa8ad58c78fd434736adfc08db9
ApoorvaRaj123 <99551949+ApoorvaRaj123@users.noreply.github.com>
2024-02-21 17:07:40 +0530

    Add data to Context.jsx

--- react 10 - Context API/src/components/Context.jsx
+++ react 10 - Context API/src/components/Context.jsx
@@ -1,8 +1,19 @@
-import React from "react";
+import { useState } from "react";
+import { createContext } from "react";
 
-const Context = (pro) => {
-    console.log(pro);
-    return <UserContext.provider>{pro.children}</UserContext.provider>
+
+export const UserContext = createContext();
+
+const Context = (props) => {
+
+    const [users, setUsers] = useState([
+        {id: 1, name: "John", email: "aa@gmail.com"},
+        {id: 2, name: "Apoo", email: "bb@gmail.com"},
+        {id: 3, name: "Yash", email: "cc@gmail.com"},
+    ])
+
+    console.log(props);
+    return <UserContext.Provider vlaue = {{users, setUsers}}>{props.children}</UserContext.Provider>;
 }
 
 export default Context;
