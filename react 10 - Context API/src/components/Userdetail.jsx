b85c747260ac01f330ffe1c8f37a9c4fbcc7fbec
ApoorvaRaj123 <99551949+ApoorvaRaj123@users.noreply.github.com>
2024-02-22 23:39:37 +0530

    Update Userdetail.jsx

--- react 10 - Context API/src/components/Userdetail.jsx
+++ react 10 - Context API/src/components/Userdetail.jsx
@@ -1,9 +1,19 @@
-import { React } from "react";
+import { React, useContext } from "react";
+import { UserContext } from "./Context";
+import {useNavigate, useParams } from "react-router-dom";
 
 const Userdetail = () => {
+
+  const {id} = useParams();
+  const navigate = useNavigate();
+  const {users} = useContext(UserContext);
+
   return (
     <>
-      <h1>Userdetails</h1>
+      <h1 className="text-4xl font-bold text-red-200">{users[id].name}</h1>
+      <h3 className="text-red-400">{users[id].email}</h3>
+      <button onClick={()=> navigate(-1)} className="rounded-md mt-5 bg-red-200 p-2">go back</button>
+
     </>
   );
 };
