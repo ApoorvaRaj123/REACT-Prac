680c719c690eee1d96e94bca77c7e90850bf0347
ApoorvaRaj123 <99551949+ApoorvaRaj123@users.noreply.github.com>
2024-02-22 23:40:05 +0530

    Update App.jsx

--- react 10 - Context API/src/App.jsx
+++ react 10 - Context API/src/App.jsx
@@ -1,26 +1,27 @@
 import React, { useState } from "react";
-import {Link, Routes, Route} from "react-router-dom";
+import { Link, Routes, Route } from "react-router-dom";
 import User from "./components/User.jsx";
 import About from "./components/About.jsx";
 import Home from "./components/Home.jsx";
+import Userdetail from "./components/Userdetail.jsx";
 
 function App() {
- 
   return (
     <>
-      <nav className="mt-10 flex justify-center gap-20">
-        <Link to="/">Home</Link>
-        <Link to="/user">User</Link>
-        <Link to="/about">About</Link>
-      </nav>
+      <div className="pt-1 container bg-red-100 m-auto">
+        <nav className="mt-10 flex justify-center gap-20">
+          <Link to="/">Home</Link>
+          <Link to="/user">User</Link>
+          <Link to="/about">About</Link>
+        </nav>
 
-      <Routes>
-        <Route path = "/" element = {<Home />} />
-        <Route path = "/user" element = {<User />} />
-        <Route path = "/about" element = {<About />} />
-      </Routes>
-
-      
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/user" element={<User />} />
+          <Route path="/user/:id" element={<Userdetail />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
     </>
   );
 }
