a7dd0722fc5183b21f840bdc39fe6a28a8a831ac
ApoorvaRaj123 <99551949+ApoorvaRaj123@users.noreply.github.com>
2024-02-23 23:16:37 +0530

    Update App.jsx

--- react 10 - Context API/src/App.jsx
+++ react 10 - Context API/src/App.jsx
@@ -19,7 +19,6 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/user" element={<User />} />
           <Route path="/user/:id" element={<Userdetail />} />
-          <Route path="/user/:id" element={<Userdetail />} />
           <Route path="/about" element={<About />} />
         </Routes>
       </div>
