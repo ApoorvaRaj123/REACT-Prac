eb9e42310cf623819625e7cabe409d08a384a45f
ApoorvaRaj123 <99551949+ApoorvaRaj123@users.noreply.github.com>
2024-04-15 16:18:19 +0530

    Add a project
    This project is made by using all the concepts learned before and also by using react-redux.

---
+++ Big project/src/App.jsx
@@ -0,0 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+
+function App() {
+
+  return (
+    <>
+      <div className="bg-[#1F1E24] h-screen w-full">
+          
+          <Routes>
+            <Route path="/" element={<Home/>} />
+          </Routes>
+      </div>
+    </>
+  );
+}
+
+export default App;
