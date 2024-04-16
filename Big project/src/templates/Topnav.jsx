223a05a28a7069e769e17bc1a1c7c65bafadf8e4
ApoorvaRaj123 <99551949+ApoorvaRaj123@users.noreply.github.com>
2024-04-16 21:38:21 +0530

    Update Topnav.jsx

--- Big project/src/templates/Topnav.jsx
+++ Big project/src/templates/Topnav.jsx
@@ -1,9 +1,49 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Topnav() {
   return (
     
-      <div className="w-[80%] h-screen bg-red-200">Topnav</div>
+      <div className="w-full h-[9vh] relative flex justify-center items-center">
+      <i class="text-zinc-400 text-2xl ri-search-line"></i>
+      <input
+        className="w-[50%] text-zinc-200 mx-10 p-5 text-xl outline-none border-none bg-transparent selection:text-zinc-900 selection:bg-white"
+        type="text"
+        placeholder="search anything"
+      />
+      <i class="text-zinc-400 text-3xl ri-close-line"></i>
+
+      <div className="w-[50%] absolute h-[50vh] bg-zinc-200 top-[90%] overflow-auto">
+        <Link className="hover:text-black hover:bg-zinc-300 duration-300 font-semibold text-zinc-600 p-10 flex justify-start items-center border-b-2 border-zinc-100">
+          <img src="" alt="" />
+          <span>Hello Everyone</span>
+        </Link>
+        <Link className="hover:text-black hover:bg-zinc-300 duration-300 font-semibold text-zinc-600 p-10 flex justify-start items-center border-b-2 border-zinc-100">
+          <img src="" alt="" />
+          <span>Hello Everyone</span>
+        </Link>
+        <Link className="hover:text-black hover:bg-zinc-300 duration-300 font-semibold text-zinc-600 p-10 flex justify-start items-center border-b-2 border-zinc-100">
+          <img src="" alt="" />
+          <span>Hello Everyone</span>
+        </Link>
+        <Link className="hover:text-black hover:bg-zinc-300 duration-300 font-semibold text-zinc-600 p-10 flex justify-start items-center border-b-2 border-zinc-100">
+          <img src="" alt="" />
+          <span>Hello Everyone</span>
+        </Link>
+        <Link className="hover:text-black hover:bg-zinc-300 duration-300 font-semibold text-zinc-600 p-10 flex justify-start items-center border-b-2 border-zinc-100">
+          <img src="" alt="" />
+          <span>Hello Everyone</span>
+        </Link>
+        <Link className="hover:text-black hover:bg-zinc-300 duration-300 font-semibold text-zinc-600 p-10 flex justify-start items-center border-b-2 border-zinc-100">
+          <img src="" alt="" />
+          <span>Hello Everyone</span>
+        </Link>
+        <Link className="hover:text-black hover:bg-zinc-300 duration-300 font-semibold text-zinc-600 p-10 flex justify-start items-center border-b-2 border-zinc-100">
+          <img src="" alt="" />
+          <span>Hello Everyone</span>
+        </Link>
+      </div>
+    </div>
    
   );
 }
