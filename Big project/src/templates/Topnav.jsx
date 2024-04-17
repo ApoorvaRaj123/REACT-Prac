9d1295d858e8a6f265c4916198b3647581943c3d
ApoorvaRaj123 <99551949+ApoorvaRaj123@users.noreply.github.com>
2024-04-17 23:41:32 +0530

    Add functionality for search in Topnav.jsx

--- Big project/src/templates/Topnav.jsx
+++ Big project/src/templates/Topnav.jsx
@@ -2,16 +2,24 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function Topnav() {
+const [query, setQuery] = useState("");
+  
   return (
-    
-      <div className="w-full h-[9vh] relative flex justify-center items-center">
-      <i class="text-zinc-400 text-2xl ri-search-line"></i>
+    <div className="w-full h-[9vh] relative flex justify-center items-center">
+      <i className="text-zinc-400 text-2xl ri-search-line"></i>
       <input
+        onChange={(e) => setQuery(e.target.value)}
+        value={query}
         className="w-[50%] text-zinc-200 mx-10 p-5 text-xl outline-none border-none bg-transparent selection:text-zinc-900 selection:bg-white"
         type="text"
         placeholder="search anything"
       />
-      <i class="text-zinc-400 text-3xl ri-close-line"></i>
+      {query.length > 0 && (
+        <i
+          onClick={() => setQuery("")}
+          className="text-zinc-400 text-3xl ri-close-line"
+        ></i>
+      )}
 
       <div className="w-[50%] absolute h-[50vh] bg-zinc-200 top-[90%] overflow-auto">
         <Link className="hover:text-black hover:bg-zinc-300 duration-300 font-semibold text-zinc-600 p-10 flex justify-start items-center border-b-2 border-zinc-100">
