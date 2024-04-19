ae2e727cc6ae2c3c6491b70aa2a3af286cbea8ad
ApoorvaRaj123 <99551949+ApoorvaRaj123@users.noreply.github.com>
2024-04-19 21:44:48 +0530

    give headers in axios.jsx

--- Big project/src/utils/axios.jsx
+++ Big project/src/utils/axios.jsx
@@ -1,7 +1,12 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "https://fakestoreapi.com/",
+  baseURL: "https://api.themoviedb.org/3/",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5YjRjYjhhZmM5NzYzY2UxZDZlYmVkOTllMGEzZjI4YSIsInN1YiI6IjY2MjI1NjVmMGQxMWYyMDE2NDAyMWZiNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.2Up6D1XuzBt1TD29Jc1_tZouejSq1QOAmJhT7d1ws4o",
+  },
 });
 
 export default instance;
