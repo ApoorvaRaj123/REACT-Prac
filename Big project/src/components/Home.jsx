10a5129f2938e18db938db3a3e2ddc9975582b44
ApoorvaRaj123 <99551949+ApoorvaRaj123@users.noreply.github.com>
2024-04-20 23:20:03 +0530

    add .get axios for header image Home.jsx

--- Big project/src/components/Home.jsx
+++ Big project/src/components/Home.jsx
@@ -1,19 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Sidenav from "../templates/Sidenav";
 import Topnav from "../templates/Topnav";
+import Header from "../templates/Header";
+import axios from "../utils/axios";
+import HorizontalCards from "../templates/HorizintalCards";
 
 function Home(params) {
   document.title = "SCSDB | Homepage";
 
-  return (
+  const [wallpaper, setWallpaper] = useState([]);
+
+  const getWallpaper = async () => {
+    try {
+      const { data } = await axios.get(`/trending/all/day`);
+      const random =
+        data.results[(Math.random() * data.results.length).toFixed()];
+      setWallpaper(random);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getWallpaper();
+  }, []);
+
+  return wallpaper ? (
     <div className="flex">
       <Sidenav />
-
       <div className="w-[80%] h-screen">
         <Topnav />
+        <Header data={wallpaper} />
+        <HorizontalCards />
       </div>
-    
     </div>
-  );
+  ):<h1>Loading</h1>
 }
 export default Home;
