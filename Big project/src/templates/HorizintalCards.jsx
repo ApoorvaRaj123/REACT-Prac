5b88b6ab03249ce05719ac90e54a28dce287cd13
ApoorvaRaj123 <99551949+ApoorvaRaj123@users.noreply.github.com>
2024-04-20 23:23:23 +0530

    Add Horizontal Cards files

---
+++ Big project/src/templates/HorizintalCards.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function HorizontalCards() {
+  return <div className="w-full h-[40vh] bg-red-300"></div>;
+
+    <div className=""></div>
+
+}
+
+export default HorizontalCards;
