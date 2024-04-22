e521ddcbfcd41c6c0aa81fe4c03a15ce5da37abb
ApoorvaRaj123 <99551949+ApoorvaRaj123@users.noreply.github.com>
2024-04-22 21:41:43 +0530

    Add select menu options in Dropdown.jsx

--- Big project/src/templates/Dropdown.jsx
+++ Big project/src/templates/Dropdown.jsx
@@ -1,14 +1,25 @@
 import React from "react";
-import { Link } from "react-router-dom";
-
-
-
 
 function Dropdown() {
-    return (
-        <div className="">Dropdown</div>
-    )
+  return (
+    <>
+      <select
+        className="rounded-xl p-2 bg-transparent text-white border-zinc-600 border-2"
+        name=""
+        id=""
+      >
+        <option className="bg-zinc-400" value="all">
+          All
+        </option>
+        <option className="bg-zinc-400" value="movies">
+          MOVIES
+        </option>
+        <option className="bg-zinc-400" value="tv">
+          TV
+        </option>
+      </select>
+    </>
+  );
 }
-
 
 export default Dropdown;
